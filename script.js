8366ec9c8d902aa669032d62fc83daea89258a8c
jur42282 <jur42282@spseol.cz>
2024-06-10 19:05:14 +0200

    added tutorials

--- script.js
+++ script.js
@@ -7,12 +7,3 @@ function hideSidebar() {
     const sidebar =document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
   }
-
-  window.addEventListener('scroll', function() {
-    var nav = document.querySelector('nav'); // Select the nav element
-    if (window.scrollY > 50) { // Check if the page is scrolled down more than 50 pixels
-        nav.classList.add('blur-background'); // Add the blur effect
-    } else {
-        nav.classList.remove('blur-background'); // Remove the blur effect if scrolled back up
-    }
-});
